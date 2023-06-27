b29d421bd8f820963afc648e2fb2788ac4a72658
dkrall <iblistrigger4@yahoo.com>
2023-06-26 20:10:45 -0700

    Added Volume Number, Description, Series Title, and Format

--- ProductCategory.h
+++ ProductCategory.h
@@ -18,6 +18,7 @@ class ProductCategory
   private:
     list<Product> product_list;
     list<string> list_of_json_files;
+    list<string> list_of_series_titles;
     void populateListOfJsonFiles();
     void populateProducts(string folderPath, bool isVerbose);
     string folderPath;
@@ -25,6 +26,10 @@ class ProductCategory
     double max_manual_isbn;
     static bool isTitleFirstInAlphabet(Product& first, Product& second);
     static bool isYN(string prompt);
+    string sanitizeString(string string_to_sanitize);
+    bool seriesTitleExists(string series_title);
+    string getSeriesTitleFromUser();
+    void printAllSeries();
 
   public:
     ProductCategory();
@@ -34,6 +39,7 @@ class ProductCategory
     void addEntry();
     void printSearchResults(list<Product> search_results, char mode);
     void searchProducts(string search_string);
+    static bool isStringFirstInAlphabet(string& first, string& second);
 };
 
 #endif
